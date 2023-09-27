948a098ee44e4ea01cb26639835a10d80b9c1a51
Alexon <s_vysochenko@mail.ru>
2023-09-27 15:56:00 +0700

    пробник 5 итоговый

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
+VK.init({
+    apiId: 51756977,
+    onlyWidgets: true,
+    insertBefore: '#someElement'
+});
+
+function authVK() {
+return new Promise((resolve, reject) => {
+    VK.Auth.login(response => {
+        if (response.session) {
+            resolve(response);
+        } else {
+            reject(new Error('Не удалось авторизоваться'));
+        }
+    });
+});
+}
 
 let a = document.querySelector('.btn');
 
