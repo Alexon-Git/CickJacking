026652b9fa589d3a0e3022706823f0b8d687f3ec
Alexon <s_vysochenko@mail.ru>
2023-09-27 15:30:53 +0700

    12345

--- App.js
+++ App.js
@@ -12,11 +12,11 @@ function authVK() {
 
 
 let a = document.querySelector('.btn');
-a.addEventListener('click', () => {
-    console.log(authVK()
-    .then(data => {
-        console.log(data);}))
-});
+// a.addEventListener('click', () => {
+//     console.log(authVK()
+//     .then(data => {
+//         console.log(data);}))
+// });
 
 a.addEventListener('click', () => {
 
