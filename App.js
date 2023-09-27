b9a05aa156dc7e7d4c46866d0397dd2a58669c36
Alexon <s_vysochenko@mail.ru>
2023-09-27 15:53:30 +0700

    пробник 5

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
 
-
-
 let a = document.querySelector('.btn');
-// a.addEventListener('click', () => {
-//     console.log(authVK()
-//     .then(data => {
-//         console.log(data);}))
-// });
 
 a.addEventListener('click', () => {
 
@@ -18,17 +11,6 @@ a.addEventListener('click', () => {
             info.innerHTML = `
                 <p>${user.first_name} ${user.last_name}</p>`;
         })
-        // .then(response => {
-        //     return VK.Api.call('users.get', { fields: 'photo_200' });
-        // })
-        // .then(user => {
-        //     let user = data.session.user;
-        //     let info = document.getElementById('user_info');
-        //     info.innerHTML = `
-        //         <img src="${user.photo_200}" width="100px"/>
-        //         <p>${user.first_name} ${user.last_name}</p>`;
-
-        // })
         .catch(error => {
             console.error(error);
         });
