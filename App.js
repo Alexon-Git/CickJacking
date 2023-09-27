9a9c864896cde0fc2e79e3b85cdb62f977574888
Alexon <s_vysochenko@mail.ru>
2023-09-27 13:01:48 +0700

    прототип 2

---
+++ App.js
@@ -0,0 +1,38 @@
+// Инициализация SDK
+VK.init({
+    apiId: API_ID
+  });
+  
+  function loginWithVK() {
+    return new Promise(function(resolve, reject) {
+      VK.Auth.login(response => {
+        if (response.session) {
+          resolve(response);  
+        } else {
+          reject(new Error('Не удалось авторизоваться'));
+        }
+      });
+    });
+  }
+  
+  // Обработка авторизации
+  loginWithVK()
+    .then(response => {
+  
+      // Запрос данных пользователя
+      return VK.Api.call('users.get', {fields: 'photo_200'});
+  
+    })
+    .then(user => {
+  
+      // Вывод данных на страницу
+      let info = document.getElementById('user_info');
+      info.innerHTML = `
+        <img src="${user[0].photo_200}" width="100px"/>
+        <p>${user[0].first_name} ${user[0].last_name}</p>
+      `;
+  
+    })
+    .catch(error => {
+      console.log(error); 
+    });
