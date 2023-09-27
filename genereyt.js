8919e2e32c342c80daae1baeb72b1fd28bba57e8
Alexon <s_vysochenko@mail.ru>
2023-09-27 17:25:48 +0700

    1234

---
+++ genereyt.js
@@ -0,0 +1,30 @@
+const client_id = 51756977;
+const redirect_uri = 'https://cick-jacking.vercel.app/';
+
+// Функция для генерации ссылки
+function generateVkAuthUrl() {
+
+  let params = {
+    client_id: client_id,
+    redirect_uri: redirect_uri,
+    display: 'popup',
+    scope: 'photos,wall',
+    response_type: 'code',
+    v: '5.92'
+  };
+
+  let queryString = Object.keys(params)
+    .map(key => key + '=' + params[key])
+    .join('&');
+
+  return 'https://oauth.vk.com/authorize?' + queryString;
+
+}
+
+// Использование
+let authUrl = generateVkAuthUrl(); 
+let info = document.getElementById('user_info');
+info.innerHTML = `authUrl`;
+
+// Переход по ссылке для начала авторизации
+// window.location = authUrl;
